3b053fd76591b50b24eb513eda584354a17da2a1
wangyuzhen <sail456852@hotmail.com>
2016-11-22 21:28:40 +0800

    I just created a keylogger to follow my Udemy tutorial

---
+++ Keylogger/Keylogger/Keylogger.cpp
@@ -0,0 +1,15 @@
+// Keylogger.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <Windows.h>
+
+using namespace std; 
+
+int main()
+{
+	
+	return 0;
+}
+
